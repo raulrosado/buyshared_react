f4e257bb47bbfda233145a970930326548474080
Raul <raulrosado91@gmail.com>
2023-01-04 13:42:49 -0600

    git init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Head from './component/Head.js';
+
+function App({ Component }) {
+  return (
+    <div className="App">
+      <Head/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Head.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@nextui-org/react';
+
+function Head() {
+  return (
+    <div>
+        
+
+    </div>
+  )
+}
+
+export default Head
