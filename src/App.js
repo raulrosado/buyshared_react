54593a8fc306919c66dbd10b158c7bb2aabadf15
Raul <raulrosado91@gmail.com>
2023-01-04 15:22:18 -0600

    se agrego el header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Head from './component/Head.js';
 function App({ Component }) {
   return (
     <div className="App">
-      <Head/>
+      <Head srcdir="https://i.pravatar.cc/150?u=a042581f4e29026024d"/>
     </div>
   );
 }
--- src/component/Head.js
+++ src/component/Head.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import { Button } from '@nextui-org/react';
+//import { Button } from '@nextui-org/react';
+import { Container, Row, Text, Col, Avatar} from "@nextui-org/react";
 
-function Head() {
+
+function Head(props) {
+  console.log(props.srcdir)
+
+  const style = {
+    padding: 10
+  };
+
   return (
-    <div>
-        
-
+    <div style={style}>
+      <Container fluid Display="flex" justify="space-between" AlignItems="flex-end" gap={0}>
+        <Row fluid align="center" justify="space-between" gap={0}>
+          <Col span={8} gap={0} style={{textAlign: '-webkit-left', display: 'flex'}} justify="center" align="center">
+            <Text 
+              h3 
+              size={15} 
+              css={{
+                textGradient: "45deg, $blue600 -20%, $pink600 50%",
+                m: 0
+              }}
+              weight="bold">
+               Hola:
+            </Text>
+            <Text
+              h1
+              size={30}
+              css={{
+                textGradient: "45deg, $purple600 -20%, $pink600 100%",
+              }}
+              weight="bold"
+            >
+              Raul
+          </Text>
+          </Col>
+          <Col span={4} justify="flex-end" AlignItems="flex-end" gap={0} style={{textAlign: '-webkit-right'}}>
+            <Avatar
+              size="lg"
+              src={props.srcdir}
+              color="primary"
+              bordered
+            />
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
