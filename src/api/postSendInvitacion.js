b9addf6183e1b93686a17762f64394742617bc00
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-02-03 23:06:44 -0600

    Add:solicitud post

--- src/api/postSendInvitacion.js
+++ src/api/postSendInvitacion.js
@@ -1,2 +1,2 @@
 import axiosInstance  from "./axios_config"
-export const postSendInvitacion = (request,header) => axiosInstance.post(`v1/api/invitation`,request,header)
+export const postSendInvitacion = (request,header) => axiosInstance.post(`v1/api/solicitudes/sendSolicitud`,request,header)
