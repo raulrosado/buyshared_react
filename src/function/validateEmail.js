4285ac350dcf7a8e20c4f8af129205ba86ce669d
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-02-12 20:14:09 -0600

    Add:Edit All information of user

---
+++ src/function/validateEmail.js
@@ -0,0 +1,5 @@
+const validateEmail = (value) => {
+    return value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+  };
+
+export default validateEmail;
--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -32,6 +32,10 @@ function Setting() {
     const [loadingPicture, setLoadingPicture] = React.useState(false);
     const [loadingInfoP, setLoadingInfoP] = React.useState(false);
     const [loadingPassword, setLoadingPassword] = React.useState(false);
+    const [texto , setTexto] = React.useState("");
+    const [color, setColor] = React.useState("");
+    const [textoEmail , setTextoEmail] = React.useState("");
+    const [colorEmail, setColorEmail] = React.useState("");
 
 
     const form = document.querySelector("form");
@@ -55,206 +59,219 @@ function Setting() {
         buttonLoadingPassword = <Loading size="sm" />
     }
 
-    const changeImagen = () =>{
+    const changeImagen = () => {
         setLoadingPicture(true)
-         header = {
+        header = {
             headers: {
-                Authorization : `Bearer ${appState.token}`,
+                Authorization: `Bearer ${appState.token}`,
                 "Content-Type": "multipart/form-data"
             }
         }
         const formData = new FormData(form);
-        postChangePictur(formData,header).then((res)=>{
+        postChangePictur(formData, header).then((res) => {
             setLoadingPicture(false)
             console.log(res)
         });
     }
 
-    const changePersonalInformation = () =>{
-        let request = {
-            name:document.getElementById("nombre"),
-            email:document.getElementById("email")
-        }
+    const changePersonalInformation = () => {
         setLoadingInfoP(true)
-        postChangeInfo(request,header).then((res)=>{
-            setLoadingInfoP(false)
-            console.log(res)
-        })
-    }
-
-    const changePassword = () =>{
-        let requestPassword = {
-            passwordAnt:document.getElementById("password"),
-            passwordNew:document.getElementById("passwordNew"),
-            passwordRepetNew:document.getElementById("passwordRepeatNew")
+        let request = {
+            name: document.getElementById("nombre").value,
+            email: document.getElementById("email").value
         }
-        setLoadingPassword(true)
-        postChangePassword(requestPassword,header).then((res)=>{
-            setLoadingPassword(false)
-            console.log(res)
+        postChangeInfo(request, header).then((res) => {
+            setLoadingInfoP(false)
         })
     }
 
-    return (
-        <div>
-            <Head />
-            <Grid.Container gap={2} justify="center">
-                <Grid xs={4}>
-                    <Link to="/main">
-                        <Avatar
-                            bordered
-                            size="md"
-                            as="button"
-                            squared
-                            icon={<LeftArroy fill="currentColor" filled />}
-                        />
-                    </Link>
-                </Grid>
-                <Grid xs={4}>
-                    <Text h3>Setting</Text>
-                </Grid>
-                <Grid xs={4}>
+    const changePassword = () => {
+        setLoadingPassword(true)
+        let requestPassword = {
+            passwordAnt: document.getElementById("password").value,
+            passwordNew: document.getElementById("passwordNew").value,
+            passwordRepetNew: document.getElementById("passwordRepeatNew").value
+        }
+        if (requestPassword.passwordNew === requestPassword.passwordRepetNew) {
+            setTexto("")
+            setColor("")
+            postChangePassword(requestPassword, header).then((res) => {
+                setLoadingPassword(false)
+            })
+        }else{
+            setTexto("Las contraseñas no coinciden")
+            setColor("error")
+        }
+    }
 
-                </Grid>
-            </Grid.Container>
-            <section style={{ padding: '10px' }}>
-                <Card css={{ p: "$6", mw: "400px" }}>
-                    <Card.Header>
-                        <Text h4 css={{ lineHeight: "$xs" }}>
-                            Cambiar Imagen
-                        </Text>
-                    </Card.Header>
-                    <Card.Body>
-                        <form name="cangePictureForm" methot="POST" encType="multipart/form-date">
-                            <Grid.Container gap={0} justify="center">
-                                <Grid xs={3}>
+
+return (
+    <div>
+        <Head />
+        <Grid.Container gap={2} justify="center">
+            <Grid xs={4}>
+                <Link to="/main">
+                    <Avatar
+                        bordered
+                        size="md"
+                        as="button"
+                        squared
+                        icon={<LeftArroy fill="currentColor" filled />}
+                    />
+                </Link>
+            </Grid>
+            <Grid xs={4}>
+                <Text h3>Setting</Text>
+            </Grid>
+            <Grid xs={4}>
+
+            </Grid>
+        </Grid.Container>
+        <section style={{ padding: '10px' }}>
+            <Card css={{ p: "$6", mw: "400px" }}>
+                <Card.Header>
+                    <Text h4 css={{ lineHeight: "$xs" }}>
+                        Cambiar Imagen
+                    </Text>
+                </Card.Header>
+                <Card.Body>
+                    <form name="cangePictureForm" methot="POST" encType="multipart/form-date">
+                        <Grid.Container gap={0} justify="center">
+                            <Grid xs={3}>
                                 <Avatar
                                     bordered
                                     size="xl"
                                     src={config.URL + "images/" + appState.user.avatar}
                                 />
-                                </Grid>
-                                <Grid xs={8}>
-                                    <Input
-                                        clearable
-                                        name="file"
-                                        id="file"
-                                        type="file"
-                                        color="primary"
-                                        size="sm"
-                                        placeholder="File" />
-                                </Grid>
-                            </Grid.Container>
-                        </form>
-                    </Card.Body>
-                    <Card.Footer>
-                        <Button onPress={changeImagen}>Cambiar imagen</Button>{buttonLoading}
-                    </Card.Footer>
-                </Card>
-            </section>
-            <section style={{ padding: '10px' }}>
-                <Card css={{ p: "$6", mw: "400px" }}>
-                    <Card.Header>
-                        <Text h4 css={{ lineHeight: "$xs" }}>
-                            Informacion Personal
-                        </Text>
-                    </Card.Header>
-                    <Card.Body>
-                        <Grid.Container gap={1} justify="center">
-                            <Grid xs={12}>
-                                <Input
-                                    id="nombre"
-                                    clearable
-                                    bordered
-                                    fullWidth
-                                    color="primary"
-                                    size="lg"
-                                    type="text"
-                                    placeholder={nombre}
-                                    contentLeft={<Usericon fill="currentColor" />}
-                                />
                             </Grid>
-                            <Grid xs={12}>
+                            <Grid xs={8}>
                                 <Input
-                                    id="email"
                                     clearable
-                                    bordered
-                                    fullWidth
+                                    name="file"
+                                    id="file"
+                                    type="file"
                                     color="primary"
-                                    size="lg"
-                                    type="email"
-                                    placeholder={appState.user.email}
-                                    contentLeft={<Mail fill="currentColor" />}
-                                />
+                                    size="sm"
+                                    placeholder="File" />
                             </Grid>
                         </Grid.Container>
-                    </Card.Body>
-                    <Card.Footer>
-                        <Button onPress={buttonLoadingInfoP}>Actualizar</Button>
-                    </Card.Footer>
-                </Card>
-            </section>
-            <section style={{ padding: '10px' }}>
-                <Card css={{ p: "$6", mw: "400px" }}>
-                    <Card.Header>
-                        <Text h4 css={{ lineHeight: "$xs" }}>
-                            Cambiar Contraseña
-                        </Text>
-                    </Card.Header>
-                    <Card.Body>
-                        <Grid.Container gap={1} justify="center">
+                    </form>
+                </Card.Body>
+                <Card.Footer>
+                    <Button onPress={changeImagen}>Cambiar imagen</Button>{buttonLoading}
+                </Card.Footer>
+            </Card>
+        </section>
+        <section style={{ padding: '10px' }}>
+            <Card css={{ p: "$6", mw: "400px" }}>
+                <Card.Header>
+                    <Text h4 css={{ lineHeight: "$xs" }}>
+                        Informacion Personal
+                    </Text>
+                </Card.Header>
+                <Card.Body>
+                    <Grid.Container gap={1} justify="center">
+                        <Grid xs={12}>
+                            <Input
+                                id="nombre"
+                                clearable
+                                bordered
+                                fullWidth
+                                color="primary"
+                                size="lg"
+                                type="text"
+                                placeholder={nombre}
+                                contentLeft={<Usericon fill="currentColor" />}
+                            />
+                        </Grid>
+                        <Grid xs={12}>
+                            <Input
+                                id="email"
+                                clearable
+                                bordered
+                                fullWidth
+                                color="primary"
+                                size="lg"
+                                type="email"
+                                status={colorEmail}
+                                color={colorEmail}
+                                helperColor={colorEmail}
+                                helperText={textoEmail}
+                                placeholder={appState.user.email}
+                                contentLeft={<Mail fill="currentColor" />}
+                            />
+                        </Grid>
+                    </Grid.Container>
+                </Card.Body>
+                <Card.Footer>
+                    <Button onPress={changePersonalInformation}>Actualizar</Button>{buttonLoadingInfoP}
+                </Card.Footer>
+            </Card>
+        </section>
+        <section style={{ padding: '10px' }}>
+            <Card css={{ p: "$6", mw: "400px" }}>
+                <Card.Header>
+                    <Text h4 css={{ lineHeight: "$xs" }}>
+                        Cambiar Contraseña
+                    </Text>
+                </Card.Header>
+                <Card.Body>
+                    <Grid.Container gap={1} justify="center">
 
-                            <Grid xs={12}>
-                                <Input.Password
-                                    id="password"
-                                    clearable
-                                    bordered
-                                    fullWidth
-                                    color="primary"
-                                    size="lg"
-                                    type="password"
-                                    placeholder="Contraseña Antigua"
-                                    visibleIcon={<UnLockIcon fill="currentColor" />}
-                                    hiddenIcon={<LockIcon fill="currentColor" />}
-                                />
-                            </Grid>
-                            <Grid xs={12}>
-                                <Input.Password
-                                    id="passwordNew"
-                                    clearable
-                                    bordered
-                                    fullWidth
-                                    color="primary"
-                                    size="lg"
-                                    type="password"
-                                    placeholder="Nueva Contraseña"
-                                    visibleIcon={<UnLockIcon fill="currentColor" />}
-                                    hiddenIcon={<LockIcon fill="currentColor" />}
-                                />
-                            </Grid>
-                            <Grid xs={12}>
-                                <Input.Password
-                                    id="passwordRepeatNew"
-                                    clearable
-                                    bordered
-                                    fullWidth
-                                    color="primary"
-                                    size="lg"
-                                    type="password"
-                                    placeholder="Repetir Contraseña"
-                                    visibleIcon={<UnLockIcon fill="currentColor" />}
-                                    hiddenIcon={<LockIcon fill="currentColor" />}
-                                />
-                            </Grid>
-                        </Grid.Container>
-                    </Card.Body>
-                    <Card.Footer>
-                        <Button onPress={buttonLoadingPassword}>Actualizar</Button>
-                    </Card.Footer>
-                </Card>
-            </section>
-        </div>
-    )
+                        <Grid xs={12}>
+                            <Input.Password
+                                id="password"
+                                clearable
+                                bordered
+                                fullWidth
+                                color="primary"
+                                size="lg"
+                                type="password"
+                                placeholder="Contraseña Antigua"
+                                visibleIcon={<UnLockIcon fill="currentColor" />}
+                                hiddenIcon={<LockIcon fill="currentColor" />}
+                            />
+                        </Grid>
+                        <Grid xs={12}>
+                            <Input.Password
+                                id="passwordNew"
+                                clearable
+                                bordered
+                                fullWidth
+                                color="primary"
+                                size="lg"
+                                type="password"
+                                placeholder="Nueva Contraseña"
+                                visibleIcon={<UnLockIcon fill="currentColor" />}
+                                hiddenIcon={<LockIcon fill="currentColor" />}
+                            />
+                        </Grid>
+                        <Grid xs={12}>
+                            <Input.Password
+                                id="passwordRepeatNew"
+                                clearable
+                                bordered
+                                fullWidth
+                                status={color}
+                                color={color}
+                                helperColor={color}
+                                helperText={texto}
+                                size="lg"
+                                type="password"
+                                placeholder="Repetir Contraseña"
+                                visibleIcon={<UnLockIcon fill="currentColor" />}
+                                hiddenIcon={<LockIcon fill="currentColor" />}
+                            />
+                        </Grid>
+                    </Grid.Container>
+                </Card.Body>
+                <Card.Footer>
+                    <Button onPress={changePassword}>Actualizar</Button>{buttonLoadingPassword}
+                </Card.Footer>
+            </Card>
+        </section>
+    </div>
+)
 }
 
 export default Setting
