892cf8a86131fca2eb7ebdc0e572c8ed7c39d57b
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-03-22 16:19:09 -0500

    Add:Rutas protegidas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Main from './pages/Main.js';
 import Detalles from './pages/Detalles.js';
@@ -10,23 +11,29 @@ import Registro from './pages/Registro'
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setUser,saveLocalStorage } from "./actions";
+import NotRegisteredUser from './pages/NotRegisteredUser';
+import { Router } from "react-router-dom";  
 
 function App({ Component }) {
   const dispatch = useDispatch();
   const user = useSelector(state => state.user);
   const navigate = useNavigate();
-  let isLogged = false
-  // if(user.token !== ""){
-  //   isLogged = false 
-  // }else{ 
-  //   isLogged = true;
-  // }
-  if(user !== null){
-    if(user.length === 0){
-      const buyshare = JSON.parse(localStorage.getItem('buyshare'));
-      dispatch(saveLocalStorage (buyshare))
+  const [isAuth, setIsAuth] = useState(false);
+  
+  useEffect(() => {
+    if(user === null){
+      setIsAuth(false)
+    }else{
+      setIsAuth(true)
+      if(user.length === 0){
+        const buyshare = JSON.parse(localStorage.getItem('buyshare'));
+        dispatch(saveLocalStorage (buyshare))
+        setIsAuth(false)
+      }else{
+        setIsAuth(true)
+      }
     }
-  }
+  }, [user])
 
   return (
     <div className="App">
@@ -36,14 +43,23 @@ function App({ Component }) {
         <Route path="/registro" element={<Registro />} />
         <Route path="/accept/:token" element={<AcceptSolicitud />} />
         <Route path="*" element={NotFound} />
+        <Route path="/not" element={<NotRegisteredUser />} />
       </Routes>
-      
-      <Routes>
-        <Route path="/detallesEventos/:id" element={<DetallesEventos />} />
-        <Route path="/detalles/:id" element={<Detalles />} />
-        <Route path="/main" element={<Main />} />
-        <Route path="/setting" element={<Setting />} />  
+      {isAuth ? (
+        <Routes>
+          <Route path="/main" exact element={<Main />} ></Route>
+          <Route path="/detallesEventos/:id" exact element={<DetallesEventos />} ></Route>
+          <Route path="/setting" exact element={<Setting />} ></Route>
+          <Route path="/detalles/:id" exact element={<Detalles />}></Route>
       </Routes>
+      ) : 
+      (<Routes>
+        <Route path="/detallesEventos/:id" exact element={<NotRegisteredUser />} />
+        <Route path="/detalles/:id" exact element={<NotRegisteredUser />} />
+        <Route path="/main" exact element={<NotRegisteredUser />} />
+        <Route path="/setting" exact element={<NotRegisteredUser />} />
+      </Routes>
+      )}
        
     </div>
   );
@@ -51,9 +67,6 @@ function App({ Component }) {
 
 function NotFound() {
   return <>Ha llegado a una página que no existe</>;
-}
-function NotAuthorized() {
-  return <>No esta autorizado estar aqui</>;
 }
 
 export default App;
